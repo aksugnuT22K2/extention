d4d961c2799131d35511f3aae738bc17acdb497c
orange <orange@test.com>
2018-10-17 17:51:54 +0900

    first

---
+++ js/background.js
@@ -0,0 +1,17 @@
+chrome.contextMenus.create({
+  title: 'barcord',
+  id: 'barcord',
+  contexts: ['all']
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === 'barcord') smAction(tab);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  smAction(tab);
+});
+
+function smAction(tab) {
+  chrome.tabs.sendMessage(tab.id, 'Action');
+}
