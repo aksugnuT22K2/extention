53f37d3d4d9ec1ecbbbb94c68dfe8c14d9943c3a
orange <orange@test.com>
2018-10-18 17:53:57 +0900

    add action button

--- js/background.js
+++ js/background.js
@@ -1,17 +1,16 @@
 chrome.contextMenus.create({
   title: 'barcord',
   id: 'barcord',
-  contexts: ['all']
+  contexts: ['page'],
+  type: 'normal'
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
   if (info.menuItemId === 'barcord') smAction(tab);
 });
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   smAction(tab);
 });
-
 function smAction(tab) {
   chrome.tabs.sendMessage(tab.id, 'Action');
 }
--- js/barcord.js
+++ js/barcord.js
@@ -1,5 +1,22 @@
+let mainFunction = function() {
+  alert(document.title);
+};
+
+const printArea = document.querySelector('#PrintBase');
+let insertElement = document.createElement('input');
+insertElement.type = 'button';
+insertElement.classList.add('non_print');
+insertElement.value = 'テスト';
+insertElement.width = '4rem';
+insertElement.style.marginLeft = '32.5rem';
+document.body
+  .insertBefore(insertElement, printArea)
+  .addEventListener('click', function() {
+    mainFunction();
+  });
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request == 'Action') {
-    alert(document.title);
+    mainFunction();
   }
 });
